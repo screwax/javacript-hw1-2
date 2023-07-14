22c26b16d086e325305515b6440bd935ca08a01e
screwax <screwaxx@gmail.com>
2023-07-14 16:31:08 +0300

    1

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const str = 'Привіт';
+const num = 123;
+const flag = true;
+const txt = "true";
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
+let html = true;
+let git = null;
+let css = undefined;
+let fruit = 2;
+let name = "Steve";
+const country = "Brazil";
+const image = BigInt;
+const animal = 230;
+const computer = false;
+const weather = "Rain";
+
+let userName = prompt("Привіт, як тебе звати?");
+console.log(userName);
+
+const Alice = 15;
+const David = 14;
+alert(Alice + David);
+
+const member = "1y_5";
+console.log("Я навчаюся в GoITeens в групі: ", member);
